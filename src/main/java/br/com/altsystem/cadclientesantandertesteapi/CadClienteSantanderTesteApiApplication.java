93656ec47189c1f14297afa9c8e68bd967ceb5f2
Altamir Caetano de Oliveira <altcaeol@gmail.com>
2023-06-02 12:07:13 -0300

    Versão inicial do Projeto

---
+++ src/main/java/br/com/altsystem/cadclientesantandertesteapi/CadClienteSantanderTesteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.altsystem.cadclientesantandertesteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadClienteSantanderTesteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadClienteSantanderTesteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/altsystem/cadclientesantandertesteapi/CadClienteSantanderTesteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.altsystem.cadclientesantandertesteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadClienteSantanderTesteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
